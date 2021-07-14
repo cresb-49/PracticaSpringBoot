27a7803aa2806cab8667a5975ce41c55ac93d313
Benjamin <carlosbpac@gmail.com>
2021-07-14 14:39:05 -0600

    Plantilla de trabajo del proyecto

---
+++ src/main/java/com/carlos/springpage/SpringpageApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.springpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/springpage/SpringpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.springpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
