7fe160d0ae7e75845e9864ab0bd10cd5f7fc0b35
Benjamin <carlosbpac@gmail.com>
2021-07-14 14:54:28 -0600

    Parametros de coneccion a la base de datos

--- src/main/java/com/carlos/springpage/SpringpageApplication.java
+++ src/main/java/com/carlos/springpage/SpringpageApplication.java
@@ -1,13 +1,21 @@
 package com.carlos.springpage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringpageApplication {
-
+public class SpringpageApplication implements CommandLineRunner{
+	private static Logger LOG = LoggerFactory.getLogger(SpringApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringpageApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("Servidor en funcionamiento");
+	}
 }
